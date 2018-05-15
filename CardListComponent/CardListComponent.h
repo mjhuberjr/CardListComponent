22342dd71de243cdb51753de85a64ce6a929c48c
Michael J. Huber Jr <info@op131.com>
2018-05-14 20:39:47 -0600

    Initial commit

---
+++ CardListComponent/CardListComponent.h
@@ -0,0 +1,19 @@
+//
+//  CardListComponent.h
+//  CardListComponent
+//
+//  Created by Michael J. Huber Jr. on 5/14/18.
+//  Copyright © 2018 operation thirteenOne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CardListComponent.
+FOUNDATION_EXPORT double CardListComponentVersionNumber;
+
+//! Project version string for CardListComponent.
+FOUNDATION_EXPORT const unsigned char CardListComponentVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CardListComponent/PublicHeader.h>
+
+
