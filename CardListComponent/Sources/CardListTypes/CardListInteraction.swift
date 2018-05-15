44f277a60198d117ee01112315cf852bd4b8acda
Michael J. Huber Jr <info@op131.com>
2018-05-14 21:00:47 -0600

    Add files and folder structure of known classes

---
+++ CardListComponent/Sources/CardListTypes/CardListInteraction.swift
@@ -0,0 +1,9 @@
+//
+//  CardListInteraction.swift
+//  CardListComponent
+//
+//  Created by Michael J. Huber Jr. on 5/14/18.
+//  Copyright © 2018 operation thirteenOne. All rights reserved.
+//
+
+import Foundation
